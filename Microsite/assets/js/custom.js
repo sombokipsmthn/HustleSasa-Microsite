215028cbec4109a6af1a71d5024337b1f9c7830a
Kipsmthn <somboriot@gmail.com>
2022-10-01 16:28:26 +0300

    Update custom.js

--- Microsite/assets/js/custom.js
+++ Microsite/assets/js/custom.js
@@ -458,15 +458,6 @@ ToogleMenu.on('click', function(event) {
 });
 
 
-/*-- Scroll JS --------------------------------------------------------------------------------------------- --*/
-
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-    });
-});
-
 /*-- View More --------------------------------------------------------------------------------------------- --*/
 
 function hideElements() {
